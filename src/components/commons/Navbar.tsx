e95777e156653496f92c7ed6064d3120b44790fd
xyleneSVG <dhavinf.a@gmail.com>
2026-03-17 15:14:10 +0700

    fix: max width

--- src/components/commons/Navbar.tsx
+++ src/components/commons/Navbar.tsx
@@ -17,52 +17,54 @@ export function NavbarComponent() {
 	return (
 		<>
 			<div className="h-20 w-full"></div>
+			<div className="bg-dark fixed bottom-0 z-999 flex w-full justify-center shadow-md">
+				<div className="flex w-full max-w-82 flex-row items-center justify-around py-4">
+					<Link href="/">
+						<House
+							className={cn(
+								"size-7 transition-colors",
+								pathname === "/" ? "text-primary" : "text-primary-foreground"
+							)}
+						/>
+					</Link>
 
-			<div className="bg-dark fixed bottom-0 z-999 flex w-full flex-row items-center justify-around p-4 shadow-md">
-				<Link href="/">
-					<House
-						className={cn(
-							"size-7 transition-colors",
-							pathname === "/" ? "text-primary" : "text-primary-foreground"
-						)}
-					/>
-				</Link>
+					<Link href="/list">
+						<NotebookTabs
+							className={cn(
+								"size-7 transition-colors",
+								pathname === "/list" ? "text-primary" : "text-primary-foreground"
+							)}
+						/>
+					</Link>
 
-				<Link href="/list">
-					<NotebookTabs
-						className={cn(
-							"size-7 transition-colors",
-							pathname === "/list" ? "text-primary" : "text-primary-foreground"
-						)}
-					/>
-				</Link>
+					<Link href="/notifications">
+						<Bell
+							className={cn(
+								"size-7 transition-colors",
+								pathname === "/notifications" ? "text-primary" : "text-primary-foreground"
+							)}
+						/>
+					</Link>
 
-				<Link href="/notifications">
-					<Bell
-						className={cn(
-							"size-7 transition-colors",
-							pathname === "/notifications" ? "text-primary" : "text-primary-foreground"
-						)}
-					/>
-				</Link>
+					<Link href="/messages">
+						<MessageCircle
+							className={cn(
+								"size-7 transition-colors",
+								pathname === "/messages" ? "text-primary" : "text-primary-foreground"
+							)}
+						/>
+					</Link>
 
-				<Link href="/messages">
-					<MessageCircle
-						className={cn(
-							"size-7 transition-colors",
-							pathname === "/messages" ? "text-primary" : "text-primary-foreground"
-						)}
-					/>
-				</Link>
+					<Link href="/profile">
+						<CircleUser
+							className={cn(
+								"size-7 transition-colors",
+								pathname === "/profile" ? "text-primary" : "text-primary-foreground"
+							)}
+						/>
+					</Link>
+				</div>
 
-				<Link href="/profile">
-					<CircleUser
-						className={cn(
-							"size-7 transition-colors",
-							pathname === "/profile" ? "text-primary" : "text-primary-foreground"
-						)}
-					/>
-				</Link>
 			</div>
 		</>
 	);
